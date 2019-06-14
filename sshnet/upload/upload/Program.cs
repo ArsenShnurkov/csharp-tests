6334320b4ad75d793f10a18e8b4b1335c7ec7bc3
ArsenShnurkov <ArsenShnurkov@users.noreply.github.com>
2019-06-14 10:54:58 +0300

    monodevelop template for console project

---
+++ sshnet/upload/upload/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace upload
+{
+    class MainClass
+    {
+        public static void Main (string [] args)
+        {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
